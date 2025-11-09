49aaf13864c79b6a7b2241d3cb306d3ca0013d75
Stepan4 <s9034541160@gmail.com>
2025-11-09 15:56:19 +0300

    refactor: перемещен process_factory.py в правильную директорию

--- backend/core/process_factory/__init__.py
+++ backend/core/process_factory/__init__.py
@@ -1,6 +1,6 @@
 """ProcessFactory для генерации кода процессов"""
 
-from backend.core.process_factory import ProcessFactory, process_factory
+from backend.core.process_factory.process_factory import ProcessFactory, process_factory
 
 __all__ = ["ProcessFactory", "process_factory"]
 
